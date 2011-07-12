e109935faf3f0b9067d3feec94c90cc3f1af1677
Tristan O'Neil <tristanoneil@gmail.com>
2011-07-12 15:18:04 -0400

    No need to pluralize this route.

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.get('/:context', function(req, res){
   model.getAll(req.params.context, function(error, data){
     var locals = {};
     locals[req.params.context] = data;
-    res.render(helper.pluralize(req.params.context) + '/index', locals);
+    res.render(req.params.context, locals);
   });
 });
 
